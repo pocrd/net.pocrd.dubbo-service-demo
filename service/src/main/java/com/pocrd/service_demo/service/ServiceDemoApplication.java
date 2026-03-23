001063e21e7705e3494b916c51d76ca977666282
pocrd <pocrd@poc>
2026-03-23 20:24:34 +0800

    [dev] update

--- service/src/main/java/com/pocrd/service_demo/service/ServiceDemoApplication.java
+++ service/src/main/java/com/pocrd/service_demo/service/ServiceDemoApplication.java
@@ -1,6 +1,7 @@
 package com.pocrd.service_demo.service;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @EnableDubbo
+@MapperScan("com.pocrd.service_demo.dao.autogen.mapper")
 public class ServiceDemoApplication {
 
     public static void main(String[] args) {
